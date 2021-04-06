8fdf641af97960d821b9b2ea8294dcfc3b97bdc2
Reviath <2006.onur@gmail.com>
2021-04-06 19:43:02 +0000

    Added 404 page.

--- main.go
+++ main.go
@@ -9,12 +9,16 @@ func main() {
 	server := gin.Default()
     server.Static("/css", "./public/css")
     server.Static("/js", "./public/js")
+    server.Static("/img", "./public/img")
 	server.LoadHTMLGlob("public/*.html")
 
 	server.GET("/", func(c *gin.Context) {
-		c.HTML(200, "index.html", gin.H{
-
-		})
+		c.HTML(200, "index.html", gin.H{})
 	})
+
+	server.NoRoute(func(c *gin.Context) {
+		c.HTML(200, "error.html", gin.H{})
+	})
+
 	server.Run("localhost:9000")
 }
